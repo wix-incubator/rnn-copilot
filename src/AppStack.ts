55f4c4dd997facf9e4613664b920f681c7db3bfe
Ethan Sharabi <ethans@wix.com>
2020-11-22 14:44:47 +0200

    Keep state of current selected tab in AppStack

--- src/AppStack.ts
+++ src/AppStack.ts
@@ -3,10 +3,15 @@ import {Navigation} from 'react-native-navigation';
 
 // const appStack: string[] = [];
 let currentVisibleComponentId: string;
+let currentTab: number = 0;
 
 Navigation.events().registerComponentDidAppearListener(({componentId /* , componentName, passProps, ...others */}) => {
   currentVisibleComponentId = componentId;
   // appStack.push(componentId);
+});
+
+Navigation.events().registerBottomTabSelectedListener(({selectedTabIndex/* , unselectedTabIndex */}) => {
+  currentTab = selectedTabIndex;
 });
 
 export function getVisibleComponentId() {
