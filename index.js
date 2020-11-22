36d86a553f4b21277dbc2667c237966db01d393e
Ethan Sharabi <ethans@wix.com>
2020-11-22 14:46:22 +0200

    add more icon releted apis to BottomTab

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import {Navigation} from 'react-native-navigation';
 import {Root, BottomTab} from 'rnn-simple';
+import {Assets, Colors} from 'react-native-ui-lib';
 import App from './example/App';
 import {name as appName} from './app.json';
 import './example/configurations';
 import './example/screens';
+import './example/assets';
 
 // Registering the same screen for two tabs
 Navigation.registerComponent(`${appName}_1`, () => App);
@@ -13,8 +15,10 @@ const mainTab = new BottomTab('main', appName);
 mainTab.withLabel('Main');
 
 const root = new Root();
-root.withBottomTab(new BottomTab('main', `${appName}_1`).withLabel('Main'));
-root.withBottomTab(new BottomTab('secondary', `${appName}_2`).withLabel('Second'));
+root.withBottomTab(new BottomTab('main', `${appName}_1`).withLabel('Main').withIcon(Assets.icons.home).withSelectedColor(Colors.blue30));
+root.withBottomTab(
+  new BottomTab('secondary', `${appName}_2`).withLabel('Second').withIcon(Assets.icons.account).withSelectedColor(Colors.blue30),
+);
 Navigation.events().registerAppLaunchedListener(() => {
   root.set();
 });
--- src/components/BottomTab.ts
+++ src/components/BottomTab.ts
@@ -1,8 +1,6 @@
-import {Layout} from 'react-native-navigation';
+import {Layout, OptionsBottomTab} from 'react-native-navigation';
 
 export default class BottomTab {
-  label?: string;
-  icon?: string;
   layout: Layout = {};
 
   constructor(id: string, screenId: string) {
@@ -17,16 +15,53 @@ export default class BottomTab {
             },
           },
         ],
-        options: {},
+        options: {bottomTab: {}},
       },
     };
   }
 
   withLabel(label: string) {
-    this.layout.stack.options.bottomTab = {
-      text: label,
-    };
+    this.layout.stack.options.bottomTab.text = label;
+    return this;
+  }
 
+  /**
+   * Set tab icon and its selected color
+   * @param icon
+   * @param selectedColor
+   */
+  withIcon(icon: OptionsBottomTab['icon'], selectedColor?: string): BottomTab {
+    this.layout.stack.options.bottomTab.icon = icon;
+    this.layout.stack.options.bottomTab.selectedIconColor = selectedColor;
+    return this;
+  }
+
+  /**
+   * Set tab icon and selected icon
+   * @param icon default icon
+   * @param selectedIcon selected icon
+   */
+  withIcons(icon: OptionsBottomTab['icon'], selectedIcon: OptionsBottomTab['icon']): BottomTab {
+    this.layout.stack.options.bottomTab.icon = icon;
+    this.layout.stack.options.bottomTab.selectedIcon = selectedIcon;
+    return this;
+  }
+
+  /**
+   * Set the selected color for the whole tab (icon and label)
+   * @param color
+   */
+  withSelectedColor(color: string) {
+    this.layout.stack.options.bottomTab.selectedIconColor = color;
+    this.layout.stack.options.bottomTab.selectedTextColor = color;
+    return this;
+  }
+
+  withOptions(options: OptionsBottomTab) {
+    this.layout.stack.options.bottomTab = {
+      ...this.layout.stack.options.bottomTab,
+      ...options,
+    };
     return this;
   }
 
