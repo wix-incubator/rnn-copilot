224cfd1bb314c04f8726dc7a8d8c2b319759e602
Ethan Sharabi <ethans@wix.com>
2020-11-23 10:14:27 +0200

    Add back icon

--- example/assets/index.ts
+++ example/assets/index.ts
@@ -2,6 +2,7 @@ import {Assets} from 'react-native-ui-lib';
 
 const assets = {
   account: require('./account.png'),
+  back: require('./back.png'),
   chevronRight: require('./chevronRight.png'),
   home: require('./home.png'),
 };
