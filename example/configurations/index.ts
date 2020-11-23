a96cf3b52e90e14663ecaa81767635ab16c66786
Ethan Sharabi <ethans@wix.com>
2020-11-23 11:51:19 +0200

    Fix TextField configuration

--- example/configurations/index.ts
+++ example/configurations/index.ts
@@ -1,6 +1,6 @@
 import {Spacings, ThemeManager} from 'react-native-ui-lib';
 
-ThemeManager.setComponentForcedTheme('TextField', {
+ThemeManager.setComponentForcedTheme('TextField', (props: any) => ({
   enableErrors: false,
-  containerStyle: {marginBottom: Spacings.s3},
-});
+  containerStyle: [{marginBottom: Spacings.s3}, props.containerStyle],
+}));
