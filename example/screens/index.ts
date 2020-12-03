6f9b038fe3c79fdd6ceb6ed2e7c96bc5798d9090
Ethan Sharabi <ethans@wix.com>
2020-12-03 13:31:39 +0200

    remove redundant ts-expect-error flags

--- example/screens/index.ts
+++ example/screens/index.ts
@@ -3,8 +3,6 @@ import PushActionsScreen from './PushActionsScreen';
 import TopBarActionsScreen from './TopBarActionsScreen';
 import RootsScreen from './RootsScreen';
 
-// @ts-expect-error
 Navigation.registerComponent('rnnsimple.PushActionsScreen', () => PushActionsScreen);
-// @ts-expect-error
 Navigation.registerComponent('rnnsimple.TopBarActionsScreen', () => TopBarActionsScreen);
 Navigation.registerComponent('rnnsimple.RootsScreen', () => RootsScreen);
