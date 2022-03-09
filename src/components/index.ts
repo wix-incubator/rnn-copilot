81bb22c22558818bef4092247f7f67e8673b3c66
talaviad5 <87114926+talaviad5@users.noreply.github.com>
2022-03-09 10:36:04 +0200

    Add support of listening to navigation events (#7)
    * Add support of listenening to navigation events
    
    * Update README.md to describe new events feature

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export {default as StatusBar} from './StatusBar';
 export {default as StaticOptions} from './StaticOptions';
 export {default as TopBar} from './TopBar';
 export {default as Root} from './Root';
+export {default as Events} from './Events';
