8edd25c299313926b25f3186f88dfce66bc4613f
Igor Gnatyuk <igorgn@wix.com>
2022-03-17 13:23:55 +0200

    config git

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,8 +8,10 @@ normalized_branch=$(echo $BUILDKITE_BRANCH | sed 's/[^a-zA-Z0-9-]/./g')
 
 if [ "$BUILDKITE_BRANCH" == "master" ];then
     npm version patch
+    npm publish
 else
-    npm version prerelease --preid $normalized_branch
+    npm version prerelease --no-git-tag-version --preid $normalized_branch
+    npm publish --tag $normalized_branch
 fi
 
-npm publish && git push
+git add -u && git commit -m"CI version bump" && git push
