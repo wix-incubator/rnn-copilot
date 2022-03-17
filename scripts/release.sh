671b5018e0c7bb08b4b5ea2916ec9e75d5d107b0
Igor Gnatyuk <igorgn@wix.com>
2022-03-17 12:59:15 +0200

    add token npmrc

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,7 @@
 
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_USER
+echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
 
 normalized_branch=$(echo $BUILDKITE_BRANCH | sed 's/[^a-zA-Z0-9-]/./g')
 
