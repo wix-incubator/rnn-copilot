5dcfdc3413ce80bf3eaedde270f8dc17d0b290af
Tal Aviad <talavia@wix.com>
2022-03-20 16:32:05 +0200

    PR fixes

--- src/components/Events.ts
+++ src/components/Events.ts
@@ -5,18 +5,24 @@ export type EventsListener = NavigationComponentListener;
 
 export default class Events {
   originComponentId: string;
-  eventSubscription: EventSubscription | null;
+  _eventSubscription: EventSubscription | null;
 
   constructor(componentId: string) {
     this.originComponentId = componentId;
-    this.eventSubscription = null;
+    this._eventSubscription = null;
   }
   withBindAllEvents(component: Component, componentId?: string) {
-    this.eventSubscription = Navigation.events().bindComponent(component, componentId);
+    this._eventSubscription = Navigation.events().bindComponent(component, componentId);
     return this;
   }
   withEvents(eventsListener: EventsListener) {
-    this.eventSubscription = Navigation.events().registerComponentListener(eventsListener, this.originComponentId);
+    this._eventSubscription = Navigation.events().registerComponentListener(eventsListener, this.originComponentId);
     return this;
+  }
+  clean() {
+    if (this._eventSubscription) {
+      this._eventSubscription.remove();
+      this._eventSubscription = null;
+    }
   }
 }
