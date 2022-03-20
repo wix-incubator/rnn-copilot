e00f9bc3889e2edd7056bd3c7d25d4fe85b7fa01
talaviad5 <87114926+talaviad5@users.noreply.github.com>
2022-03-20 16:42:01 +0200

    Add event subscription to Events object (#9)
    * Add event subscription to Events object
    
    * PR fixes

--- src/components/Events.ts
+++ src/components/Events.ts
@@ -1,20 +1,28 @@
 import {Component} from 'react';
-import {Navigation, NavigationComponentListener} from 'react-native-navigation';
+import {EventSubscription, Navigation, NavigationComponentListener} from 'react-native-navigation';
 
 export type EventsListener = NavigationComponentListener;
 
 export default class Events {
   originComponentId: string;
+  _eventSubscription: EventSubscription | null;
 
   constructor(componentId: string) {
     this.originComponentId = componentId;
+    this._eventSubscription = null;
   }
   withBindAllEvents(component: Component, componentId?: string) {
-    Navigation.events().bindComponent(component, componentId);
+    this._eventSubscription = Navigation.events().bindComponent(component, componentId);
     return this;
   }
   withEvents(eventsListener: EventsListener) {
-    Navigation.events().registerComponentListener(eventsListener, this.originComponentId);
+    this._eventSubscription = Navigation.events().registerComponentListener(eventsListener, this.originComponentId);
     return this;
+  }
+  clean() {
+    if (this._eventSubscription) {
+      this._eventSubscription.remove();
+      this._eventSubscription = null;
+    }
   }
 }
