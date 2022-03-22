9a496a561e79d97ee075b8ace89b73be1738cdc9
undefined <undefined>
2022-03-22 12:17:26 +0200

    finally

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,7 +7,6 @@ git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_USER
 git remote add deploy "https://$GIT_USER:$GIT_TOKEN@$remoteUrl"
 git checkout $BUILDKITE_BRANCH
-git remote -v
 echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
 
 normalized_branch=$(echo $BUILDKITE_BRANCH | sed 's/[^a-zA-Z0-9-]/./g')
@@ -15,12 +14,11 @@ normalized_branch=$(echo $BUILDKITE_BRANCH | sed 's/[^a-zA-Z0-9-]/./g')
 if [ "$BUILDKITE_BRANCH" == "master" ];then
     npm version patch
     npm publish
+    git push deploy
 else
     npm version prerelease --preid $normalized_branch.$BUILDKITE_BUILD_NUMBER
     npm publish --tag $normalized_branch
-    git push deploy $BUILDKITE_BRANCH
+    
 fi
-# git add package.json && git commit -m'release bump' &&
-# git add -u && git commit -m"CI version bump" && 
 
-# git remote remove deploy
+git remote remove deploy
