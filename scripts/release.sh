7e4584acc19ff830da877a7f4f1c38914673239b
undefined <undefined>
2022-03-22 11:57:47 +0200

    debug

--- scripts/release.sh
+++ scripts/release.sh
@@ -19,8 +19,9 @@ if [ "$BUILDKITE_BRANCH" == "master" ];then
 else
     npm version prerelease --no-git-tag-version --preid $normalized_branch.$BUILDKITE_BUILD_NUMBER
     npm publish --tag $normalized_branch
+    git add package.json && git commit -m'release bump' && git push deploy $BUILDKITE_BRANCH
 fi
 
 # git add -u && git commit -m"CI version bump" && 
-git add package.json && git commit -m'bump' && git push deploy $BUILDKITE_BRANCH
+
 git remote remove deploy
