a8f0abd4ec94069fd44580c184cabc3bfa4afcea
Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>
2022-09-07 12:18:53 +0300

    Migrate demo app to RN 0.68 (#16)
    * package.json updates
    
    * Random root files
    
    * Migrate Android to RN68
    
    * Fix old project names in Android files
    
    * Upgrade iOS to RN68

--- android/app/src/main/java/com/rnnsimple/MainApplication.java
+++ android/app/src/main/java/com/rnnsimple/MainApplication.java
@@ -8,7 +8,9 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.rnncopilot.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -36,15 +38,22 @@ public class MainApplication extends NavigationApplication {
         }
       };
 
+  private final ReactNativeHost mNewArchitectureNativeHost = new MainApplicationReactNativeHost(this);
+
   @Override
   public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      return mNewArchitectureNativeHost;
+    } else {
+      return mReactNativeHost;
+    }
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
-    
+    // If you opted-in for the New Architecture, we enable the TurboModule system
+    // ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
