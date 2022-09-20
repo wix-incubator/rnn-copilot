9ade3b23ad0b625c2c7cf893f2be818638730b35
Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>
2022-09-20 08:43:57 +0300

    Fix lint error

--- src/AppStack.ts
+++ src/AppStack.ts
@@ -3,6 +3,7 @@ import {Navigation} from 'react-native-navigation';
 
 // const appStack: string[] = [];
 let currentVisibleComponentId: string;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 let currentTab: number = 0;
 
 Navigation.events().registerComponentDidAppearListener(({componentId /* , componentName, passProps, ...others */}) => {
