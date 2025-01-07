a60c04229478afe9a7945b545fded8ffa14da6ee
Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>
2025-01-07 15:56:12 +0200

    Feat/root sidemenu (#27)
    * feat: add side menu support and update navigation structure
    
    * Organize packages alphabetically
    
    * Upgrade RN 73 (#26)
    
    * Upgrade RN - only ios for now
    
    * Upgrade android project to rn73
    
    * Update podfile.lock file

--- example/MainScreen.tsx
+++ example/MainScreen.tsx
@@ -1,36 +1,54 @@
-import React, {Component} from 'react';
+import React, {useEffect, useRef} from 'react';
 import View from 'react-native-ui-lib/view';
 import Text from 'react-native-ui-lib/text';
 import TouchableOpacity from 'react-native-ui-lib/touchableOpacity';
 import {Colors} from 'react-native-ui-lib/style';
 import {StyleSheet} from 'react-native';
-import {push} from 'rnn-copilot';
+import {push as pushBuilder, StaticOptions, TopBar} from 'rnn-copilot';
+import {Navigation} from 'react-native-navigation';
+// import {Constants} from 'react-native-ui-lib';
 
-class App extends Component<Screen> {
-  state = {};
-  push = push(undefined, this.props.componentId);
+function App({componentId}: Screen) {
+  const push = useRef(pushBuilder(undefined, componentId)).current;
 
-  renderItem(title: string, screenName: string) {
+  useEffect(() => {
+    Navigation.events().registerNavigationButtonPressedListener(event => {
+      if (event.buttonId === 'sideMenu') {
+        Navigation.mergeOptions(componentId, {
+          sideMenu: {
+            left: {
+              visible: true,
+              // width: Constants.screenWidth,
+            },
+          },
+        });
+      }
+    });
+  }, []);
+
+  const renderItem = (title: string, screenName: string) => {
     return (
-      <TouchableOpacity marginL-s5 paddingR-s5 paddingV-s4 style={styles.listItem} onPress={() => this.push.withScreen(screenName).go()}>
+      <TouchableOpacity marginL-s5 paddingR-s5 paddingV-s4 style={styles.listItem} onPress={() => push.withScreen(screenName).go()}>
         <Text text70R>{title}</Text>
       </TouchableOpacity>
     );
-  }
+  };
 
-  render() {
-    return (
-      <View flex paddingT-s5>
-        <Text marginL-s5 text40>
-          RNN Copilot
-        </Text>
-        {this.renderItem('TopBar & StatusBar', 'rnncopilot.TopBarActionsScreen')}
-        {this.renderItem('Push', 'rnncopilot.PushActionsScreen')}
-        {this.renderItem('Roots', 'rnncopilot.RootsScreen')}
-      </View>
-    );
-  }
+  return (
+    <View flex paddingT-s5>
+      <Text marginL-s5 text40>
+        RNN Copilot
+      </Text>
+      {renderItem('TopBar & StatusBar', 'rnncopilot.TopBarActionsScreen')}
+      {renderItem('Push', 'rnncopilot.PushActionsScreen')}
+      {renderItem('Roots', 'rnncopilot.RootsScreen')}
+    </View>
+  );
 }
+
+App.options = () => {
+  return new StaticOptions().withTopBar(new TopBar().withTitle('RNN COPILOT').withLeftButton({id: 'sideMenu', text: '='})).get();
+};
 
 const styles = StyleSheet.create({
   listItem: {
